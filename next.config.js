03605591c5dc4c7ab0c687b99abd767c8466fb89
alexanderflink <hi@alexanderflink.com>
2022-06-21 20:22:07 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async function () {
+    return [
+      {
+        source: '/source',
+        destination: '/destination',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
