3b04bbb05a0b1a861d2344429864ab759ebcbb99
alexanderflink <hi@alexanderflink.com>
2022-06-21 20:48:26 +0200

    test

--- middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export default async function middleware(req) {
-  const { cookies, geo, nextUrl } = req
-  const { locale, defaultLocale: nextDefaultLocale, origin, pathname } = nextUrl
-
-  return NextResponse.rewrite(`${origin}/rewrite`)
-}
