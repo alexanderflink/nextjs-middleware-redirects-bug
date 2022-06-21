07ef49c65912c5085170fc19582dd8bcaf0ff0f7
alexanderflink <hi@alexanderflink.com>
2022-06-21 20:33:27 +0200

    test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,6 +4,6 @@ export default async function middleware(req) {
   const { cookies, geo, nextUrl } = req
   const { locale, defaultLocale: nextDefaultLocale, origin, pathname } = nextUrl
 
-  return NextResponse.next()
-  // NextResponse.redirect(`${origin}/source`)
+  // return NextResponse.next()
+  return NextResponse.redirect(`${origin}/source`)
 }
